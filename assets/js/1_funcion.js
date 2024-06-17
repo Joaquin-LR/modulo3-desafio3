9423a6ee06ee62a345cc8e3c76df27c8363bf427
Joaquin-LR <3000joaquin@gmail.com>
2024-06-16 22:00:21 -0400

    v.1.0

---
+++ assets/js/1_funcion.js
@@ -0,0 +1,10 @@
+// DECLARACIÓN DE FUNCIÓN (COMENTADA POR OBVIAS RAZONES):
+// function example(a, b, c){
+//     return a+b+c
+// }
+
+// FUNCIÓN DE EXPRESIÓN
+let example = function(a, b, c) {
+    return a + b + c;
+};
+
---
+++ assets/js/2_arrow.js
@@ -0,0 +1,7 @@
+// DECLARACIÓN DE FUNCIÓN ANÓNIMA ASIGNADA A UNA VARIABLE (Comentada por obvias razones):
+// let suma = function(a, b){
+//     return a + b
+// }
+
+// ARROW FUNCTION:
+let suma = (a, b) => a + b;
